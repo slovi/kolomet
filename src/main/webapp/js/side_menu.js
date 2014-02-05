2916c7c77910a913b2f878d7b2eb84b809c2c214
Jiri Slovak <jiri.slovak@gmail.com>
2014-02-05 01:17:18 +0100

    added new index.jsp

---
+++ src/main/webapp/js/side_menu.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+function ToggleTypes() {
+	var TypeUl = document.getElementById("Types");
+	TypeUl.setAttribute("class","");
+	TypeUl.setAttribute("className","");
+	
+	var BrandUl = document.getElementById("Brands");
+	BrandUl.setAttribute("class","hidden");
+	BrandUl.setAttribute("className","hidden");
+		
+	var TADH = document.getElementById("TypeArrowDown");
+	TADH.setAttribute("class","");
+	TADH.setAttribute("className","");
+		
+	var BARH = document.getElementById("BrandArrowRight");
+	BARH.setAttribute("class","");
+	BARH.setAttribute("className","");
+		
+	var TARA = document.getElementById("TypeArrowRight");
+	TARA.setAttribute("class","hidden");
+	TARA.setAttribute("className","hidden");
+		
+	var BADA = document.getElementById("BrandArrowDown");
+	BADA.setAttribute("class","hidden");
+	BADA.setAttribute("className","hidden");
+	}
+	
+function ToggleBrands() {
+	var TypeUl = document.getElementById("Brands");
+	TypeUl.setAttribute("class","");
+	TypeUl.setAttribute("className","");
+	
+	var BrandUl = document.getElementById("Types");
+	BrandUl.setAttribute("class","hidden");
+	BrandUl.setAttribute("className","hidden");
+		
+	var TADH = document.getElementById("TypeArrowRight");
+	TADH.setAttribute("class","");
+	TADH.setAttribute("className","");
+		
+	var BARH = document.getElementById("BrandArrowDown");
+	BARH.setAttribute("class","");
+	BARH.setAttribute("className","");
+		
+	var TARA = document.getElementById("TypeArrowDown");
+	TARA.setAttribute("class","hidden");
+	TARA.setAttribute("className","hidden");
+	
+	var BADA = document.getElementById("BrandArrowRight");
+	BADA.setAttribute("class","hidden");
+	BADA.setAttribute("className","hidden");
+	}
