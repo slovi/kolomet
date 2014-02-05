511a44634e864caad7b2419d7f4b763371ecf8c4
Jiri Slovak <jiri.slovak@gmail.com>
2014-02-06 00:20:52 +0100

    side menu - by jquery

---
+++ src/main/webapp/js/side-menu-jquery.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+
+	$(".side-menu").each(function(index, element) {
+		
+		var header = $(".side-menu-header", element);
+		var body = $(".side-menu-body", element);
+		
+		header.click(function() {
+			toggleVisibility(body);
+			toggleVisibility($(".side-menu-arrow-on", header));
+			toggleVisibility($(".side-menu-arrow-off", header));
+		});
+		
+	});
+	
+});
+
+function toggleVisibility(element) {
+	if (element.is(":visible")) {
+		element.hide();
+	} else {
+		element.show();
+	}
+}
