c35b763abadc2f968fcc386fe9466ab3a71069d6
Jiri Slovak <jiri.slovak@gmail.com>
2014-03-16 02:57:25 +0100

    draft 1 - kolomet

--- src/main/java/cz/kolomet/repository/ProducerRepository.java
+++ src/main/java/cz/kolomet/repository/ProducerRepository.java
@@ -1,8 +1,10 @@
 package cz.kolomet.repository;
-import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import cz.kolomet.domain.codelist.Producer;
 
-@RooJpaRepository(domainType = Producer.class)
-public interface ProducerRepository {
+@Repository
+public interface ProducerRepository extends JpaRepository<Producer, Long>, JpaSpecificationExecutor<Producer>{
 }
